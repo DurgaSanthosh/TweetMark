d950c15668c85c5ce76921ea8210f4cf1f3a45fe
Sona-Martin <sonarosemartin@gmail.com>
2022-12-18 03:12:13 +0530

    ui feed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/navbar/navbar";
 import Feed from "./pages/Feed/Feed";
 import About from "./pages/about/about";
 import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
-import Feed from "./pages/Feed/Feed";
+
 
 function App() {
   return (
