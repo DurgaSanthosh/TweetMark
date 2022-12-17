61b9674f6093eb2157662d7e1a9c88df1d1f29b7
Hana Shelbin <hanashelbin@gmail.com>
2022-12-17 22:17:39 +0530

    about

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Landing from "./pages/Landing/Landing";
 import Navbar from "./components/navbar/navbar";
-//import Feed from "./pages/Feed/Feed";
+import Feed from "./pages/Feed/Feed";
+import About from "./pages/about/about";
 import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
       <Router>
         <Routes>
      <Route path='/' element={<><Navbar/><Landing/></>}/>
+     <Route path='/feed' element={<><Feed/></>}/>
+     <Route path='/about' element={<><About/></>}/>
      </Routes>
       </Router>
     </div>
