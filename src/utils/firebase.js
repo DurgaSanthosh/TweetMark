601660566a1d27ab860805f1af4235ef3faad76f
SmritiSrinivas3 <smritisrinivasneelamana@gmail.com>
2022-12-17 10:54:46 +0530

    setup

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnQS2h7oCDfERXpGdWQQFGa1AoQxdmwLc",
+  authDomain: "tweetmarkweb.firebaseapp.com",
+  projectId: "tweetmarkweb",
+  storageBucket: "tweetmarkweb.appspot.com",
+  messagingSenderId: "394296905189",
+  appId: "1:394296905189:web:19e2c2ef9479d5b846f30c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
